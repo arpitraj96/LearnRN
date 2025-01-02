55c960650d04ff1e44257d3db591b8051591cc89
Arpit <arpit360raj@gmail.com>
2025-01-03 00:42:11 +0530

    Learn about Styling

---
+++ App.jsx
@@ -0,0 +1,59 @@
+import {StyleSheet, View, Image, Text, Button, ScrollView, TouchableOpacity, Alert, Pressable, SafeAreaView} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    // <ScrollView>
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.text}>App hh yeh</Text>
+        <Image
+          style={{width: 300, height: 300,}}
+          source={{
+            uri: 'https://images.unsplash.com/photo-1734545503371-9a45282044d8?q=80&w=1950&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+          }}
+        />
+        <Button title="Press Me"></Button> 
+        <TouchableOpacity style={{padding: 10, backgroundColor: "yellow"}} onPress={() => Alert.alert("Pressed!!")}>
+          <Text style={{color:'green', fontSize: 15}}>Press Me</Text>
+        </TouchableOpacity>
+
+        <Pressable style={styles.button}>
+          <Text style={styles.btnText}>Press Me</Text>
+        </Pressable>
+      </SafeAreaView>
+    // </ScrollView>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: "#dadada",
+    // alignItems: 'center',
+    padding: 10,
+    gap: 10,
+  }, 
+  text:{
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  button:{
+    width: 150,
+    padding: 10,
+    backgroundColor: "#666",
+    justifyContent:'center',
+    alignItems: 'center',
+    borderRadius: 50,
+    marginTop: 5,
+    borderWidth: 2,
+    borderColor: 'yellow'
+  },
+  btnText:{
+    color: 'white',
+    fontSize: 18,
+    fontWeight: '500',
+  }
+});
