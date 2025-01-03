b573f30eaa666ddb1189830bd5e28cf96325e6ee
Arpit <arpit360raj@gmail.com>
2025-01-03 23:48:46 +0530

    User Input

--- App.jsx
+++ App.jsx
@@ -1,162 +1,27 @@
-import {FlatList, Image, StyleSheet, Text, View} from 'react-native';
-
-const dummy = [
-  {
-    id: 1,
-    name: 'Muskan',
-    email: 'muskan@gmail.com',
-    image: 'https://randomuser.me/api/portraits/women/1.jpg',
-  },
-  {
-    id: 2,
-    name: 'Aarav',
-    email: 'aarav@gmail.com',
-    image: 'https://randomuser.me/api/portraits/men/1.jpg',
-  },
-  {
-    id: 3,
-    name: 'Vivaan',
-    email: 'vivaan@gmail.com',
-    image: 'https://randomuser.me/api/portraits/men/2.jpg',
-  },
-  {
-    id: 4,
-    name: 'Anaya',
-    email: 'anaya@gmail.com',
-    image: 'https://randomuser.me/api/portraits/women/2.jpg',
-  },
-  {
-    id: 5,
-    name: 'Reyansh',
-    email: 'reyansh@gmail.com',
-    image: 'https://randomuser.me/api/portraits/men/3.jpg',
-  },
-  {
-    id: 6,
-    name: 'Saanvi',
-    email: 'saanvi@gmail.com',
-    image: 'https://randomuser.me/api/portraits/women/3.jpg',
-  },
-  {
-    id: 7,
-    name: 'Aarohi',
-    email: 'aarohi@gmail.com',
-    image: 'https://randomuser.me/api/portraits/women/4.jpg',
-  },
-  {
-    id: 8,
-    name: 'Krishna',
-    email: 'krishna@gmail.com',
-    image: 'https://randomuser.me/api/portraits/men/4.jpg',
-  },
-  {
-    id: 9,
-    name: 'Aditya',
-    email: 'aditya@gmail.com',
-    image: 'https://randomuser.me/api/portraits/men/5.jpg',
-  },
-  {
-    id: 10,
-    name: 'Ishita',
-    email: 'ishita@gmail.com',
-    image: 'https://randomuser.me/api/portraits/women/5.jpg',
-  },
-  {
-    id: 11,
-    name: 'Riya',
-    email: 'riya@gmail.com',
-    image: 'https://randomuser.me/api/portraits/women/6.jpg',
-  },
-  {
-    id: 12,
-    name: 'Siddharth',
-    email: 'siddharth@gmail.com',
-    image: 'https://randomuser.me/api/portraits/men/6.jpg',
-  },
-  {
-    id: 13,
-    name: 'Nisha',
-    email: 'nisha@gmail.com',
-    image: 'https://randomuser.me/api/portraits/women/7.jpg',
-  },
-  {
-    id: 14,
-    name: 'Karan',
-    email: 'karan@gmail.com',
-    image: 'https://randomuser.me/api/portraits/men/7.jpg',
-  },
-  {
-    id: 15,
-    name: 'Pooja',
-    email: 'pooja@gmail.com',
-    image: 'https://randomuser.me/api/portraits/women/8.jpg',
-  },
-  {
-    id: 16,
-    name: 'Rajesh',
-    email: 'rajesh@gmail.com',
-    image: 'https://randomuser.me/api/portraits/men/8.jpg',
-  },
-  {
-    id: 17,
-    name: 'Sneha',
-    email: 'sneha@gmail.com',
-    image: 'https://randomuser.me/api/portraits/women/9.jpg',
-  },
-  {
-    id: 18,
-    name: 'Vikram',
-    email: 'vikram@gmail.com',
-    image: 'https://randomuser.me/api/portraits/men/9.jpg',
-  },
-  {
-    id: 19,
-    name: 'Tanya',
-    email: 'tanya@gmail.com',
-    image: 'https://randomuser.me/api/portraits/women/10.jpg',
-  },
-  {
-    id: 20,
-    name: 'Deepak',
-    email: 'deepak@gmail.com',
-    image: 'https://randomuser.me/api/portraits/men/10.jpg',
-  },
-  {
-    id: 21,
-    name: 'Meera',
-    email: 'meera@gmail.com',
-    image: 'https://randomuser.me/api/portraits/women/11.jpg',
-  },
-  {
-    id: 22,
-    name: 'Rahul',
-    email: 'rahul@gmail.com',
-    image: 'https://randomuser.me/api/portraits/men/11.jpg',
-  },
-];
+import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
+import React, {useState} from 'react';
 
 const App = () => {
+  const [text, setText] = useState('');
+  // console.log("Name", text);
+  const [submittedText, setSubmittedText] = useState('');
+
+  const handleSubmit = () => {
+    // console.log("Pressed!!")
+    setSubmittedText(text);
+    setText('');
+  };
   return (
     <View style={styles.container}>
-      <FlatList
-        data={dummy}
-        renderItem={({item}) => (
-          <View style={styles.card}>
-            <Image
-              style={{width: 40, height: 40, borderRadius: 50}}
-              source={{
-                uri: item.image,
-              }}
-            />
-            <Text>{item.name}</Text>
-            <Text>{item.email}</Text>
-          </View>
-        )}
-        keyExtractor={item => item.id}
-        ItemSeparatorComponent={<View style={{height: 10}} />}
-        numColumns={2}
-        columnWrapperStyle={{gap: 10}}
+      <Text style={styles.title}>We'll learn how to handle User Input</Text>
+      <TextInput
+        placeholder="Enter a text here......"
+        style={styles.input}
+        value={text}
+        onChangeText={text => setText(text)}
       />
+      <Button title="Submit" onPress={handleSubmit} />
+      {submittedText ? <Text>Result: {submittedText}</Text> : null}
     </View>
   );
 };
@@ -165,21 +30,20 @@ export default App;
 
 const styles = StyleSheet.create({
   container: {
-    width: '100%',
-    height: '100%',
-    backgroundColor: '#6a6a6a',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    // justifyContent:'space-around',
-    // alignItems: 'center',
-  },
-  card: {
-    width: 120,
-    height: 120,
-    backgroundColor: '#6a6a',
-    borderRadius: 20,
-    padding: 5,
-    alignItems: 'center',
+    flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    gap: 10,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: '500',
+  },
+  input: {
+    padding: 5,
+    width: '100%',
+    borderWidth: 1,
+    borderRadius: 5,
   },
 });
